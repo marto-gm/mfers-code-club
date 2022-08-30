669efdecd91da2ec072a36b365b4b4020695f385
marto gm <111393458+marto-gm@users.noreply.github.com>
2022-08-30 20:04:45 +0000

    fix post mortems basic page

--- src/pages/PostMortems.jsx
+++ src/pages/PostMortems.jsx
@@ -9,14 +9,17 @@ class PostMortems extends React.Component {
       <Page>
         <Grid.Container justify="center" gap={3} mt="100px">
           <Grid xs={24} sm={20} justify="center">
-            <Text h1>lessons learned by mfers</Text>
+            <Text h1>lessons learned</Text>
+          </Grid>
+          <Grid xs={24} sm={20} justify="center">
+            <Text h2> 1 mfer = 1 mfer </Text>
+
           </Grid>
           <Grid xs={20} sm={20} justify="center">
             <Button>
               <Link to="/">take me home mfer</Link>
             </Button>
           </Grid>
-          <p> 1 mfer = 1 mfer </p>
         </Grid.Container>
       </Page>
     );
